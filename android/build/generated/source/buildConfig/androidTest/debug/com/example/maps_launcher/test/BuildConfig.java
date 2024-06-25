9a7d2140b20b3b3d9a40af2e13fc4d9978db7ef7
Ertadis <ertadis@hotmail.com>
2024-06-25 15:09:40 +0300

    update

---
+++ android/build/generated/source/buildConfig/androidTest/debug/com/example/maps_launcher/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.maps_launcher.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.maps_launcher.test";
+  public static final String BUILD_TYPE = "debug";
+}
